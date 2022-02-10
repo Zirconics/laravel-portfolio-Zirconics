cb03906a607f23c916c6b3a07ed7224a9d74291e
Kevin <k.oreel@student.avans.nl>
2022-02-10 11:43:46 +0100

    Finished assignment 1.32

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FaqController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,20 +19,7 @@ use App\Http\Controllers\BlogController;
 |
 */
 
-Route::get('/posts/{post}', function ($post) {
-    $posts = [
-        'my-first-post' => 'Hello, this is my first blog post!',
-        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    ];
-
-    if (!array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found.');
-    }
-
-    return view('post', [
-        'post' => $posts[$post]
-    ]);
-});
+Route::get('/posts/{post}', [PostController::class, 'show']);
 
 // Index page
 Route::get('/', [WelcomeController::class, 'show']);
