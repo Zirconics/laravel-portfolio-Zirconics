5d2473e979a82dbd7b18aab9a0682ddd3f1e5a68
Kevin <k.oreel@student.avans.nl>
2022-02-10 12:40:07 +0100

    Added doc comment to PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use App\Models\Post;
 class PostController
 {
 
+    /**
+     * Method that returns the posts from the database.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function show($slug)
     {
         return view('post', [
