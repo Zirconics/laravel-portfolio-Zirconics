e2893b8dad3859109df80233fd0a38ebe4d130a6
Kevin <k.oreel@student.avans.nl>
2022-02-13 15:39:52 +0100

    ESLint fix

--- database/migrations/2022_02_13_124530_create_grades_table.php
+++ database/migrations/2022_02_13_124530_create_grades_table.php
@@ -17,9 +17,14 @@ class CreateGradesTable extends Migration
             $table->bigIncrements('id');
             $table->string('course_name');
             $table->string('test_name');
-            $table->decimal('lowest_passing_grade')->default(5.5)->comment('Lowest grade to pass so average calculations can be applied');
-            $table->decimal('best_grade')->nullable();
-            $table->date('passed_at')->nullable()->comment('Date that the grade was larger than or equal to the lowest passing grade');
+            $table->decimal('lowest_passing_grade')
+                ->default(5.5)
+                ->comment('Lowest grade to pass so average calculations can be applied');
+            $table->decimal('best_grade')
+                ->nullable();
+            $table->date('passed_at')
+                ->nullable()
+                ->comment('Date that the grade was larger than or equal to the lowest passing grade');
             $table->timestamps();
         });
     }
