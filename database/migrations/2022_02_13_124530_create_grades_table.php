10edb4a88e81b2f6c6ab9d1f30d0b1168017fbf9
Kevin <k.oreel@student.avans.nl>
2022-02-13 13:53:10 +0100

    Finished assignment 1.44

---
+++ database/migrations/2022_02_13_124530_create_grades_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('course_name');
+            $table->string('test_name');
+            $table->decimal('lowest_passing_grade');
+            $table->decimal('best_grade');
+            $table->date('passed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
