91b2b55f7ac010cb58a74f83f180f4476e4b8791
Kevin <k.oreel@student.avans.nl>
2022-03-06 15:07:39 +0100

    Assignment 3.22

--- database/migrations/2022_02_22_124531_create_grades_table.php
+++ database/migrations/2022_02_22_124531_create_grades_table.php
@@ -15,15 +15,13 @@ class CreateGradesTable extends Migration
     {
         Schema::create('grades', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('course_name');
             $table->string('test_name');
-            $table->decimal('ec');
             $table->decimal('lowest_passing_grade')
                 ->default(5.5)
                 ->comment('Lowest grade to pass so average calculations can be applied');
             $table->decimal('best_grade')
                 ->nullable();
-            $table->date('passed_at')
+            $table->timestamp('passed_at')
                 ->nullable()
                 ->comment('Date that the grade was larger than or equal to the lowest passing grade');
             $table->timestamps();
