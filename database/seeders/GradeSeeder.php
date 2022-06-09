2974a9aa66d5f0ced513ab29927c954b3acc084e
Kevin <k.oreel@student.avans.nl>
2022-06-09 13:57:28 +0200

    pls

--- database/seeders/GradeSeeder.php
+++ database/seeders/GradeSeeder.php
@@ -16,77 +16,77 @@ class GradeSeeder extends Seeder
     {
         DB::table('grades')-> insert([
             [
-                'course_id' => '0',
+                'course_id' => '1',
                 'test_name' => 'Assessment',
                 'best_grade' => 9.8,
             ],
             [
-                'course_id' => '1',
+                'course_id' => '2',
                 'test_name' => 'Case Study',
                 'best_grade' => 7.7,
             ],
             [
-                'course_id' => '2',
+                'course_id' => '3',
                 'test_name' => 'Case Study',
                 'best_grade' => 9,
             ],
             [
-                'course_id' => '3',
+                'course_id' => '4',
                 'test_name' => 'Case Study',
                 'best_grade' => 9.5,
             ],
             [
-                'course_id' => '4',
+                'course_id' => '5',
                 'test_name' => 'Project',
                 'best_grade' => 8.5,
             ],
             [
-                'course_id' => '5',
+                'course_id' => '6',
                 'test_name' => 'Case Study',
                 'best_grade' => 0
             ],
             [
-                'course_id' => '6',
-                'test_name' => 'Project',
-                'best_grade' => 0
-            ],
-            [
                 'course_id' => '7',
-                'test_name' => 'Assessment',
+                'test_name' => 'Project',
                 'best_grade' => 0
             ],
             [
                 'course_id' => '8',
-                'test_name' => 'Report',
-                'best_grade' => 0
-            ],
-            [
-                'course_id' => '9',
-                'test_name' => 'Portfolio',
-                'best_grade' => 0
-            ],
-            [
-                'course_id' => '10',
-                'test_name' => 'Project',
-                'best_grade' => 0
-            ],
-            [
-                'course_id' => '11',
                 'test_name' => 'Assessment',
                 'best_grade' => 0
             ],
             [
-                'course_id' => '12',
+                'course_id' => '9',
+                'test_name' => 'Report',
+                'best_grade' => 0
+            ],
+            [
+                'course_id' => '10',
                 'test_name' => 'Portfolio',
+                'best_grade' => 0
+            ],
+            [
+                'course_id' => '11',
+                'test_name' => 'Project',
+                'best_grade' => 0
+            ],
+            [
+                'course_id' => '12',
+                'test_name' => 'Assessment',
                 'best_grade' => 0
             ],
             [
                 'course_id' => '13',
                 'test_name' => 'Portfolio',
-                'best_grade' => 0,
+                'best_grade' => 0
             ],
             [
                 'course_id' => '14',
+                'test_name' => 'Portfolio',
+                'best_grade' => 0,
+            ],
+            [
+                'course_id' => '15',
                 'test_name' => 'Portfolio',
                 'best_grade' => 0,
             ]
