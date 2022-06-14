29612205b15c785df0388dbec0e92714f52d8182
Kevin <k.oreel@student.avans.nl>
2022-06-14 11:03:39 +0200

    fixed form messages + updated bootstrap to v5.2

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -14,12 +14,15 @@
 
     <div class="container">
         <a href={{ route('articles.index') }}><button class="btn btn-light" id="blog_buttons">Cancel</button></a>
-        <div class="row g4 justify-content-center">
-            <form class="row g-3 was-validated" method="POST" action={{ route('articles.index') }}>
+        <div class="row g-3 justify-content-center">
+            <form class="row g-3 needs-validation" method="POST" action="{{ route('articles.index') }}" novalidate>
                 @csrf
                 <div class="col-md-12">
                     <label for="validationCustom01" class="form-label h1-white-bold">Title:</label>
                     <input type="text" class="form-control" id="title" placeholder="Enter title" name="title" required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
                     <div class="invalid-feedback">
                         Please provide a title.
                     </div>
@@ -27,6 +30,9 @@
                 <div class="col-md-12">
                     <label for="validationCustom02" class="form-label h1-white-bold">Excerpt:</label>
                     <input type="text" class="form-control" id="excerpt" placeholder="Enter excerpt" name="excerpt" required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
                     <div class="invalid-feedback">
                         Please provide an excerpt.
                     </div>
@@ -34,6 +40,9 @@
                 <div class="col-md-12">
                     <label for="validationCustom02" class="form-label h1-white-bold">Body:</label>
                     <textarea type="text" class="form-control form-input" id="body" placeholder="Enter body" name="body" required></textarea>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
                     <div class="invalid-feedback">
                         Please provide a body.
                     </div>
@@ -45,4 +54,5 @@
         </div>
     </div>
 @endsection
+
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+          crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css" />
 
     <title>{{ $title }}</title>
@@ -81,9 +83,30 @@
     @yield('content')
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
         crossorigin="anonymous"></script>
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+</script>
 @yield('script')
 </body>
 
